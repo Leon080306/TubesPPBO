d287bbbc18e28332a341119879e80c7808abfe1c
Leonard Samuel Setiawan <leonsamuels080306@gmail.com>
2025-11-18 10:29:13 +0700

    fix database not working

--- src/main/java/utils/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class AppConfig {
-    public static final String DATABASE = "hotel_harapan_bangsa";
-    public static final String PORT = "5432";
-    public static final String HOST = "localhost";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "Leon080306";
-}
