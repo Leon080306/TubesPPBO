f2b9b887c25d94fac19e427f5973829e1a7e162b
Leonard Samuel Setiawan <leonsamuels080306@gmail.com>
2025-11-24 12:55:50 +0700

    Create MainFrame.java

---
+++ src/main/java/views/MainFrame.java
@@ -0,0 +1,39 @@
+package views;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("Hotel Harapan Bangsa Management System");
+
+        int width = 800;
+        int height = 600;
+
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createMenuBar();
+
+        setLayout(new BorderLayout());
+        setVisible(true);
+    }
+
+    private void createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Menu");
+        JMenuItem exitItem = new JMenuItem("Logout");
+
+        exitItem.addActionListener(e -> {
+            dispose();
+            new LoginView();
+        });
+
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+    }
+}
