1e74f315f7bc70a72d92c03863adcb992f06b5a2
Leonard Samuel Setiawan <leonsamuels080306@gmail.com>
2025-12-13 23:53:13 +0700

    add usermanagement button

--- src/main/java/views/admin/AdminMainMenu.java
+++ src/main/java/views/admin/AdminMainMenu.java
@@ -4,6 +4,7 @@ import models.Admin;
 import views.MainFrame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AdminMainMenu {
     MainFrame frame;
@@ -14,6 +15,28 @@ public class AdminMainMenu {
     }
 
     private void renderAdminMainMenu() {
-        frame.setVisible(true); // Don't forget this!
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        JLabel title = new JLabel("ADMIN MAIN MENU");
+        title.setFont(new Font("Poppins", Font.BOLD, 32));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(title);
+        mainPanel.add(Box.createVerticalStrut(50));
+
+        JButton userManagementButton = new JButton("User Management");
+        userManagementButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        userManagementButton.addActionListener(e -> {
+            frame.dispose();
+            new UserManagementView();
+        });
+
+        mainPanel.add(userManagementButton);
+
+        frame.addComponent(mainPanel);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AdminMainMenu();
     }
 }
